d9a6badc854e7f15a2831a1108c2a8896685b7d2
yabets <yabetsbedlu@gmail.com>
2025-06-29 11:54:05 +0300

    added the minimax algorithm and winner checker method

--- main.py
+++ main.py
@@ -90,3 +90,37 @@ def get_winning_line(player, check_board=board):
     if check_board[0][2] == check_board[1][1] == check_board[2][0] == player:
         return ((width, button_area), (0, button_area + board_size))
     return None
+#winner checker
+
+def check_win(player, check_board=board):
+    return get_winning_line(player, check_board) is not None
+
+#we use  Minimax AI to play the game logic
+def minimax(minimax_board, depth, is_maximizing):
+    if check_win(2, minimax_board):
+        return float('inf')
+    elif check_win(1, minimax_board):
+        return float('-inf')
+    elif is_board_full(minimax_board):
+        return 0
+
+    if is_maximizing:
+        best_score = -float('inf')
+        for row in range(board_rows):
+            for col in range(board_cols):
+                if minimax_board[row][col] == 0:
+                    minimax_board[row][col] = 2
+                    score = minimax(minimax_board, depth + 1, False)
+                    minimax_board[row][col] = 0
+                    best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = float('inf')
+        for row in range(board_rows):
+            for col in range(board_cols):
+                if minimax_board[row][col] == 0:
+                    minimax_board[row][col] = 1
+                    score = minimax(minimax_board, depth + 1, True)
+                    minimax_board[row][col] = 0
+                    best_score = min(score, best_score)
+        return best_score
